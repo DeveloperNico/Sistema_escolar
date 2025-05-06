4b1042a4f2468f917e234a4d95067d04f8223423
Nicolas Duarte <nickboytube0418@gmail.com>
2025-05-06 11:19:19 -0300

    Ajustando o css

--- Front-end/src/components/Menu/Menu.jsx
+++ Front-end/src/components/Menu/Menu.jsx
@@ -1,24 +1,28 @@
 import styles from './Menu.module.css';
+import { GraduationCap } from 'lucide-react';
+import { User } from 'lucide-react';
+import { CalendarDays } from 'lucide-react';
+import { FileChartColumnIncreasing } from 'lucide-react';
 
 export function Menu() {
     return (
         <div className={styles.container}>
-            <table>
-                <tr>
-                    <td>
-                        Prefessores
-                    </td>
-                    <td>
-                        Gestores
-                    </td>
-                    <td>
-                        Disciplina
-                    </td>
-                    <td>
-                        Ambiente
-                    </td>
-                </tr>
-            </table>
+            <div className={`${styles.card} ${styles.card1}`}>
+                <a href="#">Usuários</a>
+                <User className={styles.user}/>
+            </div>
+            <div className={`${styles.card} ${styles.card2}`}>
+                <a href="#">Professores</a>
+                <GraduationCap className={styles.graduationCap}/>
+            </div>
+            <div className={`${styles.card} ${styles.card3}`}>
+                <a href="#">Disciplinas</a>
+                <FileChartColumnIncreasing className={styles.fileChart}/>
+            </div>
+            <div className={`${styles.card} ${styles.card4}`}>
+                <a href="#">Ambientes <br/> reservados</a>
+                <CalendarDays className={styles.calendarDays}/>
+            </div>
         </div>
     )
 }
