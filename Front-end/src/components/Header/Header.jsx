c1cd7395b820a2752f5c98c0740933ade0c24943
Nicolas Duarte <nickboytube0418@gmail.com>
2025-05-06 10:32:49 -0300

    Ajustando o front-end

--- Front-end/src/components/Header/Header.jsx
+++ Front-end/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export function Header() {
     return (
         <header className={styles.container}>
-            <h1>Esolca do Nico</h1>
+            <h1>Escola do Nico</h1>
         </header>
     )
 }
