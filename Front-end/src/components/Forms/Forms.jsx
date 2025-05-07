e0ce03414350f8571dbdd8c65ef6be28d872480e
Nicolas Duarte Silva <nickboytube0418@gmail.com>
2025-05-07 11:29:28 -0300

    Estilizando a página de login

--- Front-end/src/components/Forms/Forms.jsx
+++ Front-end/src/components/Forms/Forms.jsx
@@ -1,18 +1,19 @@
 import styles from "./Forms.module.css";
+import { BookOpenText } from 'lucide-react';
 
 export function Forms() {
     return (
-        <div>
-            <form action="">
-                <div>
-                    <label htmlFor="">oi</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">oi</label>
-                    <input type="text" />
-                </div>
-            </form>
-        </div>
+        <form className={styles.form}>
+            <BookOpenText className={styles.icon}/>
+            <div className={styles.username}>
+                <label>Nome de usuário:</label>
+                <input type="text" />
+            </div>
+            <div className={styles.password}>
+                <label>Senha:</label>
+                <input type="text" />
+            </div>
+            <button>Entrar</button>
+        </form>
     );
 };
--- Front-end/src/pages/Login.jsx
+++ Front-end/src/pages/Login.jsx
@@ -1,10 +1,8 @@
-import { NavBar } from "../components/NavBar/NavBar";
 import { Forms } from "../components/Forms/Forms";
 
 export function Login() {
     return (
         <>
-            <NavBar />
             <Forms />
         </>
     );
